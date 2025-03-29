47e5252164aa4ee8965c1c8b547fbcec828d40da
kanyebek <koshonovkanybek@gmail.com>
2025-03-29 20:08:57 +0600

    HW is done

--- db/queries.py
+++ db/queries.py
@@ -1,17 +1,20 @@
-# queries.py
-
 CREATE_TABLE_TASKS = """
-    CREATE TABLE IF NOT EXISTS task (
+    CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         task TEXT NOT NULL,
-        created_at TEXT NOT NULL
-    )
+        completed INTEGER DEFAULT 0
+        )
 """
 
-SELECT_TASKS = "SELECT id, task, created_at FROM task"
+SELECT_TASKS = "SELECT id, task, completed FROM tasks"
 
-INSERT_TASK = "INSERT INTO task (task, created_at) VALUES (?, ?)"
+INSERT_TASK = "INSERT INTO tasks (task) VALUES (?)"
 
-UPDATE_TASK = "UPDATE task SET task = ?, created_at = ? WHERE id = ?"
+UPDATE_TASK = "UPDATE tasks SET task = ? WHERE id = ?"
 
-DELETE_TASK = "DELETE FROM task WHERE id = ?"
+DELETE_TASK = "DELETE FROM tasks WHERE id = ?"
+
+
+SELECT_completed = 'SELECT id, task, completed FROM tasks WHERE completed = 1'
+
+SELECT_incomplete = 'SELECT id, task, completed FROM tasks WHERE completed = 0'
--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import flet as ft
 from db import main_db
-from datetime import datetime
 
 
 def main(page: ft.Page):
     page.title = 'Todo List'
-    page.padding = 40
-    page.bgcolor = ft.Colors.GREY_800
     page.theme_mode = ft.ThemeMode.DARK
+    page.window_maximized = True 
 
     task_list = ft.Column(spacing=10)
 
+    filter_type = "all"
+
+
     def load_tasks():
         task_list.controls.clear()
-        for task_id, task_text, created_at in main_db.get_tasks():
-            task_list.controls.append(create_task_row(task_id, task_text, created_at))
+        for task_id, task_text, completed in main_db.get_tasks(filter_type):
+            task_list.controls.append(create_task_row(task_id, task_text, completed))
         page.update()
 
-    def create_task_row(task_id, task_text, created_at):
+    def create_task_row(task_id, task_text, completed):
         task_field = ft.TextField(value=task_text, expand=True, dense=True, read_only=True)
+        task_checkbox = ft.Checkbox(
+            value=bool(completed), 
+            on_change=lambda e: toggle_task(task_id, e.control.value)
+            )
 
         def enable_edit(e):
             task_field.read_only = False
@@ -27,32 +32,85 @@ def main(page: ft.Page):
         def save_edit(e):
             main_db.update_task_db(task_id, task_field.value)
             task_field.read_only = True
-
-            load_tasks()
+            page.update()
 
         return ft.Row([
+            task_checkbox,
             task_field,
-            ft.Text(created_at, size=12),
-            ft.IconButton(ft.Icons.EDIT, icon_color=ft.Colors.YELLOW_400, on_click=enable_edit),
-            ft.IconButton(ft.Icons.SAVE, icon_color=ft.Colors.GREEN_400, on_click=save_edit)
+            ft.IconButton(ft.icons.EDIT, icon_color=ft.colors.YELLOW_400, on_click=enable_edit),
+            ft.IconButton(ft.icons.SAVE, icon_color=ft.colors.GREEN_400, on_click=save_edit),
+            ft.IconButton(ft.icons.DELETE, icon_color=ft.colors.RED_400, on_click=lambda e: delete_task(task_id))
         ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
     
     def add_task(e):
-        if task_input.value.strip():
+        if task_input.value.strip() and len(task_input.value) <= 100:
+            task_input.error_text = None
             task_id = main_db.add_task_db(task_input.value)
-            task_list.controls.append(create_task_row(task_id, task_input.value, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+            task_list.controls.append(create_task_row(task_id, task_input.value))
             task_input.value = ""
             page.update()
+        else:
+            task_input.error_text = "Введите задачу (макс. 100 символов)"
+            page.update()
+
+    def toggle_task(task_id, is_completed):
+        main_db.update_task_db(task_id, completed=int(is_completed))
+        load_tasks()
+
+    def delete_task(task_id):
+        main_db.delete_task_db(task_id)
+        load_tasks()
+        
+    
+    def set_filter(filter_value):
+        nonlocal filter_type 
+
+        filter_type = filter_value
+        load_tasks()
+
 
     task_input = ft.TextField(hint_text='Добавьте задачу', expand=True, dense=True, on_submit=add_task)
-    add_button = ft.ElevatedButton("Добавить", on_click=add_task, icon=ft.Icons.ADD)
+    add_button = ft.ElevatedButton("Добавить", on_click=add_task, icon=ft.icons.ADD)
 
-    page.add(
-        ft.Column([
+    filter_button = ft.Row([
+        ft.ElevatedButton("Все", on_click=lambda e: set_filter("all")),
+        ft.ElevatedButton("Выполненные", on_click=lambda e: set_filter("completed")),
+        ft.ElevatedButton("Невыполненные", on_click=lambda e: set_filter("incompleted"))
+    ], alignment=ft.MainAxisAlignment.CENTER)
+
+    # page.add(
+    #     ft.Column([
+    #         ft.Row([task_input, add_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+    #         task_list
+    #     ])
+    # )
+
+    content = ft.Container(
+        content = ft.Column([
             ft.Row([task_input, add_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+            filter_button,
             task_list
-        ])
+        ], alignment=ft.MainAxisAlignment.CENTER), 
+        padding=20,
+        alignment=ft.alignment.center
     )
+
+    background_image = ft.Image(
+        src='/home/kanybek/Desktop/todo/image copy.png',
+        fit=ft.ImageFit.FILL,
+        width=page.width,
+        height=page.height
+    )
+
+    background = ft.Stack([background_image, content])
+
+    def on_resize(e):
+        background_image.width = page.width
+        background_image.height = page.height
+        page.update()
+
+    page.add(background)
+    page.on_resized = on_resize
 
     load_tasks()
 
