b5a060273377e1b688d525843c67d8fb7301d336
kanyebek <koshonovkanybek@gmail.com>
2025-03-31 15:15:14 +0600

    HW5

--- db/queries.py
+++ db/queries.py
@@ -1,20 +1,17 @@
+# queries.py
+
 CREATE_TABLE_TASKS = """
-    CREATE TABLE IF NOT EXISTS tasks (
+    CREATE TABLE IF NOT EXISTS task (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         task TEXT NOT NULL,
-        completed INTEGER DEFAULT 0
-        )
+        created_at TEXT NOT NULL
+    )
 """
 
-SELECT_TASKS = "SELECT id, task, completed FROM tasks"
+SELECT_TASKS = "SELECT id, task, created_at FROM task"
 
-INSERT_TASK = "INSERT INTO tasks (task) VALUES (?)"
+INSERT_TASK = "INSERT INTO task (task, created_at) VALUES (?, ?)"
 
-UPDATE_TASK = "UPDATE tasks SET task = ? WHERE id = ?"
+UPDATE_TASK = "UPDATE task SET task = ?, created_at = ? WHERE id = ?"
 
-DELETE_TASK = "DELETE FROM tasks WHERE id = ?"
-
-
-SELECT_completed = 'SELECT id, task, completed FROM tasks WHERE completed = 1'
-
-SELECT_incomplete = 'SELECT id, task, completed FROM tasks WHERE completed = 0'
+DELETE_TASK = "DELETE FROM task WHERE id = ?"
