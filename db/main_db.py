f4591d9d19d7dd5a1d8efefc6af0191126eb55f1
kanyebek <koshonovkanybek@gmail.com>
2025-03-31 16:40:37 +0600

    HW7

--- db/main_db.py
+++ db/main_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 from config import DB_PATH
 from db import queries
-from datetime import datetime
+
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
@@ -11,35 +11,53 @@ def init_db():
     conn.close()
 
 
-def get_tasks():
+def get_tasks(filter_type="all"):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute(queries.SELECT_TASKS)
+    # cursor.execute(queries.SELECT_TASKS)
+
+    if filter_type == 'completed':
+        cursor.execute(queries.SELECT_completed)
+    elif filter_type == "incomplete":
+        cursor.execute(queries.SELECT_incomplete)
+    else:
+        cursor.execute(queries.SELECT_TASKS)
+
     tasks = cursor.fetchall()
     conn.close()
     return tasks
 
 
-def add_task_db(task_text):
-    created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def add_task_db(task):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute(queries.INSERT_TASK, (task_text , created_at))
+    cursor.execute(queries.INSERT_TASK, (task,))
     conn.commit()
     task_id = cursor.lastrowid
     conn.close()
     return task_id
 
 
-def update_task_db(task_id, new_task):
-    created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# def update_task_db(task_id, new_task):
+#     conn = sqlite3.connect(DB_PATH)
+#     cursor = conn.cursor()
+#     cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
+#     conn.commit()
+#     conn.close()
+
+
+def update_task_db(task_id, new_task=None, completed=None):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute(queries.UPDATE_TASK, (new_task, created_at, task_id))
+    # cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
+
+    if new_task is not None:
+        cursor.execute("UPDATE tasks SET task = ? WHERE id = ?", (new_task, task_id))
+    if completed is not None:
+        cursor.execute("UPDATE tasks SET completed = ? WHERE id = ?", (completed, task_id))
+
     conn.commit()
     conn.close()
-
-
 
 def delete_task_db(task_id):
     conn = sqlite3.connect(DB_PATH)
@@ -48,6 +66,12 @@ def delete_task_db(task_id):
     conn.commit()
     conn.close()
 
+def delete_completed_tasks():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(queries.DELETE_COMPLETED)
+    conn.commit()
+    conn.close()
     
 
-    
+# pip install flet[all]
